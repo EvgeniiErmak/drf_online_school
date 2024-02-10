cf7f0fd8869f8dabbac39c4b997afad46c991ff1
Евгений Ермак <144560375+EvgeniiErmak@users.noreply.github.com>
2024-02-10 11:18:45 +0300

    Вариант № 3 - ввёл поправки наставника

--- materials/views.py
+++ materials/views.py
@@ -1,7 +1,12 @@
 # materials/views.py
-from rest_framework import generics
-from .models import Lesson
-from .serializers import LessonSerializer
+from rest_framework import viewsets, generics
+from .models import Course, Lesson
+from .serializers import CourseSerializer, LessonSerializer
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
 
 
 class LessonListCreateView(generics.ListCreateAPIView):
