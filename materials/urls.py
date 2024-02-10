504bf0492cf0cb723717d5cd7726c3c5974098e1
Евгений Ермак <144560375+EvgeniiErmak@users.noreply.github.com>
2024-02-10 16:19:12 +0300

    Вариант № 3 - ввёл поправки наставника

--- materials/urls.py
+++ materials/urls.py
@@ -1,9 +1,11 @@
 # materials/urls.py
 from rest_framework.routers import DefaultRouter
-from .views import CourseViewSet, LessonViewSet
+from .views import CourseViewSet, LessonListCreateView, LessonRetrieveUpdateDestroyView
 
 router = DefaultRouter()
-router.register(r'courses', CourseViewSet)
-router.register(r'lessons', LessonViewSet)
+router.register(r'courses', CourseViewSet, basename='course')
+# Используем as_view для преобразования generic-классов в view функции
+router.register(r'lessons', LessonListCreateView.as_view(), basename='lesson-list')
+router.register(r'lessons', LessonRetrieveUpdateDestroyView.as_view(), basename='lesson-detail')
 
 urlpatterns = router.urls
--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,6 @@ from rest_framework.routers import DefaultRouter
 from .views import UserProfileViewSet
 
 router = DefaultRouter()
-router.register(r'profile', UserProfileViewSet)
+router.register(r'profile', UserProfileViewSet, basename='user-profile')
 
 urlpatterns = router.urls
