bc2006472f0230c0686366e38a4616f5c925eccf
Евгений Ермак <144560375+EvgeniiErmak@users.noreply.github.com>
2024-02-11 16:06:15 +0300

    Выполнил правки наставника. Отредактировал urls

--- materials/urls.py
+++ materials/urls.py
@@ -1,13 +1,21 @@
 # materials/urls.py
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from .views import CourseViewSet, LessonListCreateView, LessonRetrieveUpdateDestroyView, HomeView
 
 app_name = 'materials'
 
+# Создаем экземпляр DefaultRouter
+router = DefaultRouter()
+
+# Регистрируем CourseViewSet под именем 'courses'
+router.register(r'courses', CourseViewSet, basename='course')
+
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('courses/', CourseViewSet.as_view({'get': 'list', 'post': 'create'}), name='course-list'),
-    path('courses/<int:pk>/', CourseViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}), name='course-detail'),
     path('lessons/', LessonListCreateView.as_view(), name='lesson-list'),
     path('lessons/<int:pk>/', LessonRetrieveUpdateDestroyView.as_view(), name='lesson-detail'),
+
+    # Включаем роутер в urlpatterns
+    path('', include(router.urls)),
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,16 @@
 # users/urls.py
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from .views import UserProfileViewSet
 
 app_name = 'users'
 
+# Создаем экземпляр DefaultRouter
+router = DefaultRouter()
+
+# Регистрируем UserProfileViewSet под именем 'user-profile'
+router.register(r'user-profile', UserProfileViewSet, basename='user-profile')
+
 urlpatterns = [
-    path('user-profile/', UserProfileViewSet.as_view({'get': 'list', 'post': 'create'}), name='user-profile'),
-    path('user-profile/<int:pk>/', UserProfileViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}), name='user-profile-detail'),
+    path('', include(router.urls)),
 ]
