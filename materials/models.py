b10a5b57d259f45156c3a303c9e2d97c60d184d3
Евгений Ермак <144560375+EvgeniiErmak@users.noreply.github.com>
2024-02-11 16:16:18 +0300

    Указал в моделях, у полей preview атрибуты null = True и blank=True

--- materials/models.py
+++ materials/models.py
@@ -4,13 +4,13 @@ from django.db import models
 
 class Course(models.Model):
     title = models.CharField(max_length=255)
-    preview = models.ImageField(upload_to='previews/')
+    preview = models.ImageField(upload_to='previews/', null=True, blank=True)
     description = models.TextField()
 
 
 class Lesson(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    preview = models.ImageField(upload_to='previews/')
+    preview = models.ImageField(upload_to='previews/', null=True, blank=True)
     video_link = models.URLField()
     course = models.ForeignKey(Course, related_name='lessons', on_delete=models.CASCADE)
