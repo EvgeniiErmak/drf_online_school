4a4bdf9286f004d199a95c56f02860054dadc22c
Евгений Ермак <144560375+EvgeniiErmak@users.noreply.github.com>
2024-02-12 04:06:03 +0300

    Выполнил Дополнительное задание

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,16 @@
 # users/serializers.py
 from rest_framework import serializers
+from materials.models import Payment
 from .models import CustomUser
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
+        fields = '__all__'
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
         fields = '__all__'
