503206f5ed78a17d5a1d3633edc2078a208ce654
Евгений Ермак <144560375+EvgeniiErmak@users.noreply.github.com>
2024-02-12 03:35:12 +0300

    Выполнил задание №3

--- materials/serializers.py
+++ materials/serializers.py
@@ -3,18 +3,20 @@ from rest_framework import serializers
 from .models import Course, Lesson
 
 
-class CourseSerializer(serializers.ModelSerializer):
-    lessons_count = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Course
-        fields = '__all__'
-
-    def get_lessons_count(self, obj):
-        return obj.lessons.count()
-
-
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    lessons = LessonSerializer(many=True, read_only=True)  # Добавляем вложенный сериализатор для уроков
+
+    lessons_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Course
+        fields = ['id', 'title', 'preview', 'description', 'lessons', 'lessons_count']
+
+    def get_lessons_count(self, obj):
+        return obj.lessons.count()
