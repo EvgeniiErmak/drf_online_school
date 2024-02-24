68d628b6a5ace36b3591818dbe733c3ab614231a
Евгений Ермак <144560375+EvgeniiErmak@users.noreply.github.com>
2024-02-24 19:44:47 +0300

    Редакция группы "Пользователи" - #4

--- drf_online_school/urls.py
+++ drf_online_school/urls.py
@@ -1,13 +1,10 @@
 # drf_online_school/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('materials/', include('materials.urls')),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 # users/urls.py
-from django.urls import path, include
+from django.urls import path
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import UserProfileViewSet
 
 app_name = 'users'
@@ -15,4 +16,6 @@ urlpatterns = [
     path('user-profile/', UserProfileViewSet.as_view({'get': 'list', 'post': 'create'}), name='user-profile'),
     path('user-profile/<int:pk>/', UserProfileViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}), name='user-profile-detail'),
     path('register/', UserProfileViewSet.as_view({'post': 'register'}), name='user-register'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
