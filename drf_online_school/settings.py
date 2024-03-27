11c9de5f4dd80db7b149b147ad7f777738dc2427
Евгений Ермак <144560375+EvgeniiErmak@users.noreply.github.com>
2024-03-27 18:02:01 +0300

    Спрятал чувствительные данные БД

--- drf_online_school/settings.py
+++ drf_online_school/settings.py
@@ -93,16 +93,15 @@ WSGI_APPLICATION = 'drf_online_school.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'drf_online_school',
-        'USER': 'postgres',
-        'PASSWORD': '12345',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": os.getenv("DB_ENGINE"),
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
